1b3f9d5be0dd77f92199ddfdd533e54f921d5977
sainu <sainu@users.noreply.github.com>
2023-01-09 23:29:35 +0900

    Setting public path for storybook and skip optimization for next/image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 import '../src/styles/globals.css';
+import Image from 'next/image';
+
+/**
+ * @see https://chuckwebtips.hatenablog.com/entry/2022/08/15/190337
+ */
+Image.propTypes = {
+  unoptimized: null,
+};
+Image.defaultProps = {
+  unoptimized: true,
+};
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
