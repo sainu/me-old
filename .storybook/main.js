150f6615f66b1d7102d7a5af563afd42794e9373
sainu <sainu@users.noreply.github.com>
2023-01-27 23:13:36 +0900

    Add alias to webpack of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,6 +18,13 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
   },
   staticDirs: ['../public', '../src/shared/test/public'],
 };
