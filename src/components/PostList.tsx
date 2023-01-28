8de94b91baebbb353b91dfba4083462831f964da
sainu <sainu@users.noreply.github.com>
2023-01-28 16:12:45 +0900

    Delete old PostList

--- src/components/PostList.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-type Props = {
-  gapClass?: string;
-};
-
-export const PostList: FC<Props> = ({ gapClass = 'gap-1', children }) => {
-  return <div className={`flex flex-col ${gapClass}`}>{children}</div>;
-};
